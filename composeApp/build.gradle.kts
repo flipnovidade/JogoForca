5a47564c6836d63d3d732b9de662af02da748d0e
Felippe Ferreira <felippe.ferreira@bbnk.com.br>
2026-02-26 21:47:38 +0000

    add txt ad mob

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -99,8 +99,8 @@ android {
         applicationId = "com.game.forca.flipsofts"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
     }
     packaging {
         resources {
